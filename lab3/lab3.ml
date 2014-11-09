bdedae79f8c33884122c386b46c5af31a285899b
bumbleblym <bumbleblym@gmail.com>
2014-11-10 01:03:59 +0800

    feat(lab3): improve algorithm with memorisation

--- lab3/lab3.ml
+++ lab3/lab3.ml
@@ -35,9 +35,57 @@ let score (xs: string) (ys: string) =
       max r1 (max r2 r3)
   in aux 0 0;;
 
-let ans1 = score "A" "A";;
-let ans2 = score "A" "AB";;
-let ans3 = score "GATCGGCAT" "CAATGTGAATC";;
+let score_mem (xs: string) (ys: string) =
+  let pts_space = -2 in
+  let x_len = String.length xs in
+  let y_len = String.length ys in
+  let ht = Hashtbl.create 10 in
+  let revise
+      (acc: int * string * string)
+      (pts: int)
+      (x_char: char)
+      (y_char: char) : int * string * string =
+    let (score, xs, ys) = acc in
+    (pts + score, String.make 1 x_char ^ xs, String.make 1 y_char ^ ys)
+  in let rec aux (xi: int) (yi: int) =
+    try Hashtbl.find ht (xi, yi)
+    with
+    | _ ->
+        if xi = x_len && yi == y_len then
+          let result = (0, "", "") in
+          Hashtbl.add ht (xi, yi) result;
+          result;
+        else if (yi == y_len) then
+          let num = x_len - xi in
+          let pts = pts_space * num in
+          let xs' = String.sub xs xi num in
+          let ys' = String.make num ' ' in
+          let result = (pts, xs', ys') in
+          Hashtbl.add ht (xi, yi) result;
+          result;
+        else if (xi == x_len) then
+          let num = y_len - yi in
+          let pts = pts_space * num in
+          let xs' = String.make num ' ' in
+          let ys' = String.sub ys yi num in
+          let result = (pts, xs', ys') in
+          Hashtbl.add ht (xi, yi) result;
+          result;
+        else
+          let x_char = String.get xs xi in
+          let y_char = String.get ys yi in
+          let r1 = revise (aux xi (yi + 1)) pts_space ' ' y_char in
+          let r2 = revise (aux (xi + 1) yi) pts_space x_char ' ' in
+          let m = mt x_char y_char in
+          let r3 = revise (aux (xi + 1) (yi + 1)) m x_char y_char in
+          let result = max r1 (max r2 r3) in
+          Hashtbl.add ht (xi, yi) result;
+          result;
+  in aux 0 0;;
+
+let ans1 = score_mem "A" "A";;
+let ans2 = score_mem "A" "AB";;
+let ans3 = score_mem "GATCGGCAT" "CAATGTGAATC";;
 
 let pprint (result: int * string * string) : unit =
   let alignment (xs: string) (ys: string) : string =
