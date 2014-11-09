18e84ab6e8659d20eedd9ac2ef0661954aeaaf2a
bumbleblym <bumbleblym@gmail.com>
2014-11-10 00:38:36 +0800

    feat(lab3): change score to return score and alignment

--- lab3/lab3.ml
+++ lab3/lab3.ml
@@ -1,28 +1,40 @@
-let mt (x:char) (y:char) = if x=y then 1 else -1
- 
-let score (xs:string) (ys:string) =
-    let x_len = String.length xs in
-    let y_len = String.length ys in
-    let _ = print_endline ("xs len :"^(string_of_int x_len)) in
-    let _ = print_endline ("ys len :"^(string_of_int y_len)) in
- 
-    let rec aux (xi:int) (yi:int) =
-      if xi=x_len && yi==y_len then 0
-      else if (yi==y_len) then (x_len-xi)*(-2)
-      else if (xi==x_len) then (y_len-yi)*(-2)
-      else
-        let r1 = -2+aux xi (yi+1) in
-        let r2 = -2+aux (xi+1) (yi) in
-        let m = mt (String.get xs xi) (String.get ys yi) in
-        let r3 = m+aux (xi+1) (yi+1) in
-        max r1 (max r2 r3)
- 
+let mt (x: char) (y: char) = if x = y then 1 else -1;;
+
+let score (xs: string) (ys: string) =
+  let pts_space = -2 in
+  let x_len = String.length xs in
+  let y_len = String.length ys in
+  let revise
+      (acc: int * string * string)
+      (pts: int)
+      (x_char: char)
+      (y_char: char) : int * string * string =
+    let (score, xs, ys) = acc in
+    (pts + score, String.make 1 x_char ^ xs, String.make 1 y_char ^ ys)
+  in let rec aux (xi: int) (yi: int) =
+    if xi = x_len && yi == y_len then (0, "", "")
+    else if (yi == y_len) then
+      let num = x_len - xi in
+      let pts = pts_space * num in
+      let xs' = String.sub xs xi num in
+      let ys' = String.make num ' ' in
+      (pts, xs', ys') 
+    else if (xi == x_len) then
+      let num = y_len - yi in
+      let pts = pts_space * num in
+      let xs' = String.make num ' ' in
+      let ys' = String.sub ys yi num in
+      (pts, xs', ys')
+    else
+      let x_char = String.get xs xi in
+      let y_char = String.get ys yi in
+      let r1 = revise (aux xi (yi + 1)) pts_space ' ' y_char in
+      let r2 = revise (aux (xi + 1) yi) pts_space x_char ' ' in
+      let m = mt x_char y_char in
+      let r3 = revise (aux (xi + 1) (yi + 1)) m x_char y_char in
+      max r1 (max r2 r3)
   in aux 0 0;;
- 
+
 let ans1 = score "A" "A";;
 let ans2 = score "A" "AB";;
 let ans3 = score "GATCGGCAT" "CAATGTGAATC";;
- 
-print_endline(string_of_int ans1);;
-print_endline(string_of_int ans2);;
-print_endline(string_of_int ans3);;
